4820e5e559d96c798e673d75438f75c57152e27d
Amr Moussa <seamrmoussa@gmail.com>
2026-04-12 06:04:50 +0200

    New SSR to Cloudfalre

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,15 @@
-import { AngularAppEngine, createRequestHandler } from '@angular/ssr';
-import { getContext } from '@netlify/angular-runtime/context.mjs';
+import { AngularAppEngine } from '@angular/ssr';
 
-const angularAppEngine = new AngularAppEngine();
+const angularApp = new AngularAppEngine();
 
-export async function netlifyAppEngineHandler(request: Request): Promise<Response> {
-  const context = getContext();
-
-  const result = await angularAppEngine.handle(request, context);
-  return result || new Response('Not found', { status: 404 });
-}
-
-/**
- * The request handler used by the Angular CLI (dev-server and during build).
- */
-export const reqHandler = createRequestHandler(netlifyAppEngineHandler);
+export default {
+  async fetch(request: Request): Promise<Response> {
+    try {
+      const res = await angularApp.handle(request);
+      return res || new Response('Page not found.', { status: 404 });
+    } catch (error) {
+      console.error('SSR Error:', error);
+      return new Response('Internal Server Error', { status: 500 });
+    }
+  },
+};
