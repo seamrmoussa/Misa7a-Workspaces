0796ad1f67cc0c21254e2c9d8cd94af21234bd9f
Amr Moussa <seamrmoussa@gmail.com>
2026-05-02 21:01:00 +0300

    new

--- src/app/core/auth/services/auth.service.ts
+++ src/app/core/auth/services/auth.service.ts
@@ -10,19 +10,19 @@ import { Router } from '@angular/router';
 export class AuthService {
   private readonly http = inject(HttpClient);
   private readonly router = inject(Router);
-  private readonly ngrokHeaders = new HttpHeaders({
-    'ngrok-skip-browser-warning': 'true',
-  });
+  // private readonly ngrokHeaders = new HttpHeaders({
+  //   'ngrok-skip-browser-warning': 'true',
+  // });
 
   signUp(data: object): Observable<any> {
     return this.http.post(environment.baseUrl + '/api/v1/users/register', data, {
-      headers: this.ngrokHeaders,
+      // headers: this.ngrokHeaders,
     });
   }
 
   signIn(data: object): Observable<any> {
     return this.http.post(environment.baseUrl + '/api/v1/auth/login', data, {
-      headers: this.ngrokHeaders,
+      // headers: this.ngrokHeaders,
     });
   }
 
