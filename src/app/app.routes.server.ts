43410557503e1ed0f41a52682bb6317b481f77bc
Amr Moussa <seamrmoussa@gmail.com>
2026-09-16 20:59:35 +0300

    new v: add payment confirmation

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'booking',
+    renderMode: RenderMode.Client, // لن يحاول Angular بناء الصفحة مسبقاً على السيرفر
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender,
   },
